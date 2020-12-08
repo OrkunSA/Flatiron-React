389e8b554fbfbcdf0e0b55060c2e7dfbf5c1497a
OrkunSA <c.orkun.s@gmail.com>
2020-12-08 17:52:52 -0600

    adding solution

--- react-dynamic-components-lab-onl01-seng-pt-041320/src/ColorBox.js
+++ react-dynamic-components-lab-onl01-seng-pt-041320/src/ColorBox.js
@@ -9,10 +9,15 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    const newOpacity = this.props.opacity - 0.1;
-    return this.props.opacity < 0.2 ? null : (<div className="color-box" style={{opacity: newOpacity + 0.1}}>
-      <ColorBox opacity={newOpacity} />
-    </div>)
+    if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+        <ColorBox opacity={this.props.opacity - 0.1} />
+      </div>
+      )
+    } else {
+      return null
+    }
   }
 }
 
