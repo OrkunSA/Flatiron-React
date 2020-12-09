5d0e78d6c45748b1dba79e33e8e954dd548a3052
OrkunSA <c.orkun.s@gmail.com>
2020-12-09 12:41:38 -0600

    adding solution

--- react-props-movie-lab-onl01-seng-pt-041320/src/MovieShowcase.js
+++ react-props-movie-lab-onl01-seng-pt-041320/src/MovieShowcase.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((item) => (
+      <MovieCard
+        title={item.title}
+        IMDBRating={item.IMDBRating}
+        genres={item.genres}
+        poster={item.poster}
+      />
+    ));
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
--- react-props-movie-lab-onl01-seng-pt-041320/src/card-components/CardBack.js
+++ react-props-movie-lab-onl01-seng-pt-041320/src/card-components/CardBack.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import zero from '../assets/stars/0-stars.png'
-import one from '../assets/stars/1-stars.png'
-import two from '../assets/stars/2-stars.png'
-import three from '../assets/stars/3-stars.png'
-import four from '../assets/stars/4-stars.png'
-import five from '../assets/stars/5-stars.png'
+import React, { Component } from "react";
+import zero from "../assets/stars/0-stars.png";
+import one from "../assets/stars/1-stars.png";
+import two from "../assets/stars/2-stars.png";
+import three from "../assets/stars/3-stars.png";
+import four from "../assets/stars/4-stars.png";
+import five from "../assets/stars/5-stars.png";
 
-const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
+const imgMapper = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five };
 
 export default class CardBack extends Component {
-
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
+    if (this.props.IMDBRating !== null) {
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />;
+    } else {
+      return <h4>No Rating Found</h4>;
+    }
+  };
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement()}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
-    )
+    );
   }
 }
