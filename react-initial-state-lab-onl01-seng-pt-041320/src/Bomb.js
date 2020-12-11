d750dbd56d7ef1317a4e9fdf2f2e0baf3d2266f5
OrkunSA <c.orkun.s@gmail.com>
2020-12-11 16:15:09 -0600

    adding solution

--- react-initial-state-lab-onl01-seng-pt-041320/src/Bomb.js
+++ react-initial-state-lab-onl01-seng-pt-041320/src/Bomb.js
@@ -1,1 +1,20 @@
-// your Bomb code here!
+import React from "react";
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      secondsLeft: props.initialCount,
+    };
+  }
+
+  render() {
+    const text =
+      this.state.secondsLeft === 0
+        ? "Boom!"
+        : `${this.state.secondsLeft} seconds left before I go boom!`;
+    return <div>{text}</div>;
+  }
+}
+
+export default Bomb;
