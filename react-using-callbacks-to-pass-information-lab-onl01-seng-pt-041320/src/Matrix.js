4461bfcb10e1fdaeda86f26de23e87d10bde9099
OrkunSA <c.orkun.s@gmail.com>
2020-12-15 14:21:52 -0600

    adding solution

--- react-using-callbacks-to-pass-information-lab-onl01-seng-pt-041320/src/Matrix.js
+++ react-using-callbacks-to-pass-information-lab-onl01-seng-pt-041320/src/Matrix.js
@@ -1,34 +1,44 @@
-import React, { Component } from 'react';
-import learnSymbol from './data.js'
-import Cell from './Cell.js'
-import ColorSelector from './ColorSelector.js'
+import React, { Component } from "react";
+import learnSymbol from "./data.js";
+import Cell from "./Cell.js";
+import ColorSelector from "./ColorSelector.js";
 
 export default class Matrix extends Component {
-
   constructor() {
-    super()
+    super();
+    this.state = {
+      selectedColor: "#FFF",
+    };
   }
 
-  genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
-  )
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor,
+    });
+  };
 
-  genMatrix = () => (
-    this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
-  )
+  genRow = (vals) =>
+    vals.map((val, idx) => (
+      <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />
+    ));
+
+  genMatrix = () =>
+    this.props.values.map((rowVals, idx) => (
+      <div key={idx} className="row">
+        {this.genRow(rowVals)}
+      </div>
+    ));
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
-        </div>
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
+        <div id="matrix">{this.genMatrix()}</div>
       </div>
-    )
+    );
   }
 }
 
 Matrix.defaultProps = {
-  values: learnSymbol
-}
+  values: learnSymbol,
+};
