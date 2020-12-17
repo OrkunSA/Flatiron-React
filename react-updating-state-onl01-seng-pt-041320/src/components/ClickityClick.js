fc89bc62c89be6d02160542e77f63e50a3d9130e
OrkunSA <c.orkun.s@gmail.com>
2020-12-16 20:54:28 -0600

    adding solution

--- react-updating-state-onl01-seng-pt-041320/src/components/ClickityClick.js
+++ react-updating-state-onl01-seng-pt-041320/src/components/ClickityClick.js
@@ -1,1 +1,30 @@
-// Code ClickityClick Component Here
+import React from "react";
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hansBeenClicked: false,
+      currentTheme: "blue",
+    };
+  }
+
+  handleClick = () => {
+    this.setState(
+      {
+        hansBeenClicked: true,
+      },
+      () => console.log(this.state.hansBeenClicked)
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hansBeenClicked ? null : "not"} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
+
+export default ClickityClick;
