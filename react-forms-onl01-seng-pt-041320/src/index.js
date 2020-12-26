88602c989c0f8e9e54837c340f8ebed5b2207f61
OrkunSA <c.orkun.s@gmail.com>
2020-12-25 19:05:56 -0600

    Adding solution

--- react-forms-onl01-seng-pt-041320/src/index.js
+++ react-forms-onl01-seng-pt-041320/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+import ParentComponent from "./components/ParentComponent";
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
