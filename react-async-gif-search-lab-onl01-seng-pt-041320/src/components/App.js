0e3b1642e2d9db19116d7ad6dcbff84d2cae6720
OrkunSA <c.orkun.s@gmail.com>
2020-12-27 17:55:21 -0600

    Adding lab

---
+++ react-async-gif-search-lab-onl01-seng-pt-041320/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import NavBar from './NavBar'
+
+// the App component should render out the GifListContainer component 
+
+const App = () => {
+  return (
+    <div>
+        < NavBar color='black' title="Giphy Search" />
+    </div>
+  )
+}
+
+export default App
