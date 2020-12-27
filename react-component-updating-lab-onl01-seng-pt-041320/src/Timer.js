68b8679862a3f53b42d70bf33776c26cc89b94da
OrkunSA <c.orkun.s@gmail.com>
2020-12-27 17:07:21 -0600

    Adding solution

--- react-component-updating-lab-onl01-seng-pt-041320/src/Timer.js
+++ react-component-updating-lab-onl01-seng-pt-041320/src/Timer.js
@@ -6,7 +6,7 @@ class Timer extends Component {
     this.timer = React.createRef();
     this.state = {
       time: 0,
-      color: "#" + Math.floor(Math.random() * 16777215).toString(16)
+      color: "#" + Math.floor(Math.random() * 16777215).toString(16),
     };
   }
 
@@ -17,6 +17,18 @@ class Timer extends Component {
       this.clockTick,
       this.props.updateInterval * 1000
     );
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false;
+    }
+    return true;
   }
 
   componentWillUnmount() {
@@ -36,8 +48,8 @@ class Timer extends Component {
   }
 
   clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time + this.props.updateInterval
+    this.setState((prevState) => ({
+      time: prevState.time + this.props.updateInterval,
     }));
   };
 
