99f6f1c1bae3652de01608ca7edfa03d4a2c82d3
OrkunSA <c.orkun.s@gmail.com>
2021-01-04 21:05:21 -0600

    adding solution

--- react-components-as-routes-onl01-seng-pt-041320/src/index.js
+++ react-components-as-routes-onl01-seng-pt-041320/src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
 };
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
