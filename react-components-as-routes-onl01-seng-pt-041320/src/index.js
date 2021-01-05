78f3e82503e7c29cf25d13a1553f065d1f74bd01
OrkunSA <c.orkun.s@gmail.com>
2021-01-04 20:35:02 -0600

    adding lab

---
+++ react-components-as-routes-onl01-seng-pt-041320/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
