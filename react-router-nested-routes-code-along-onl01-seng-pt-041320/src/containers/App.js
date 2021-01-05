547f3700b4c351b43c6c0d7b4cf3315377342c1c
OrkunSA <c.orkun.s@gmail.com>
2021-01-04 22:16:30 -0600

    adding solution

--- react-router-nested-routes-code-along-onl01-seng-pt-041320/src/containers/App.js
+++ react-router-nested-routes-code-along-onl01-seng-pt-041320/src/containers/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import MoviesPage from './MoviesPage';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import MoviesPage from "./MoviesPage";
 
 class App extends Component {
-
   state = {
     movies: {
-      1: { id: 1, title: 'A River Runs Through It' },
-      2: { id: 2, title: 'Se7en' },
-      3: { id: 3, title: 'Inception' }
-    }
-  }
+      1: { id: 1, title: "A River Runs Through It" },
+      2: { id: 2, title: "Se7en" },
+      3: { id: 3, title: "Inception" },
+    },
+  };
 
   render() {
     return (
@@ -22,7 +18,12 @@ class App extends Component {
         <div>
           <NavBar />
           <Route exact path="/" render={() => <div>Home</div>} />
-          <Route path='/movies' render={routerProps => <MoviesPage {...routerProps} movies={this.state.movies}/>} />
+          <Route
+            path="/movies"
+            render={(routerProps) => (
+              <MoviesPage {...routerProps} movies={this.state.movies} />
+            )}
+          />
         </div>
       </Router>
     );
